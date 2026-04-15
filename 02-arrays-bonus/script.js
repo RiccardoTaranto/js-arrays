d3f8f9cf6e38634089cc867dcfb8459b4743b8a9
Riccardo <riccardo.taranto@icloud.com>
2026-04-15 10:24:26 +0200

    link items of an array in a string with commas

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -33,13 +33,13 @@ console.log(teachers);
 // ✅ 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
 let isFabioPresent = false;
-for (let i =0; i <teachers.length; i++) {
-  if (teachers[i] === 'Fabio'){
-    isFabioPresent= true;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === 'Fabio') {
+    isFabioPresent = true;
   }
 }
 console.log(isFabioPresent);
 
-
-// 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+// ✅ 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
+const teachersString = teachers(', ');
+console.log(teachersString);
